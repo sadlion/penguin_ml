587bcb5831afb6b8bf22d156b88a5c02ec169380
sadlion <sadlion@naver.com>
2021-12-30 00:37:19 +0900

    Update title2

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -11,7 +11,7 @@ import pickle
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-st.title('Penguin Classifier: A Machine Learning App'')
+st.title('Penguin Classifier: A Machine Learning App')
 st.write("This app uses 6 inputs to predict the species of penguin using "
          "a model built on the Palmer's Penguin's dataset. Use the form below"
          " to get started!")
