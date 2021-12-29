bf611208ded4b43384afbbace2a851305a6fc08c
sadlion <sadlion@naver.com>
2021-12-30 00:43:22 +0900

    Add pasword checking

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -15,6 +15,11 @@ st.title('Penguin Classifier: A Machine Learning App')
 st.write("This app uses 6 inputs to predict the species of penguin using "
          "a model built on the Palmer's Penguin's dataset. Use the form below"
          " to get started!")
+
+password_guess = st.text_input('What is the Password?')
+if password_guess != 'sadlion':
+    st.stop()
+
 penguin_file = st.file_uploader('Upload your own penguin data')
 penguin_df = None
 if penguin_file is None:
